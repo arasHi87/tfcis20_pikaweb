f3b03b3ee018de5f4f8330c0f2997c9ae77c3ad9
arasi <arasi27676271@gmail.com>
2018-02-21 15:05:04 +0800

    with config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('_system_name', 'Tfcis20 Pika');
+define('_DB_HOST', 'localhost');
+define('_DB_ID', 'root');
+define('_DB_PW', 'e27676271');
+define('_DB_NAME', 'tfcispika');
+
+$topadmin_array = ['godarasi2767'];
+$admin_array = ['arasi2767', 'testarasi'];
+//error_reporting(0);
