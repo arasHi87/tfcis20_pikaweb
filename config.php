8afc45db2cf21051760e4ec35eac76a059f725a8
root <root@DESKTOP-TEST.localdomain>
2018-03-09 08:52:07 +0800

    delete no use thing

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 define('_system_name', 'Tfcis20 Pika');
-define('_DB_HOST', 'sql213.web.youp.ga');
-define('_DB_ID', 'yuweb_21438112');
-define('_DB_PW', 'wy3p8jsb');
-define('_DB_NAME', 'yuweb_21438112_pika');
+define('_DB_HOST', 'localhost');
+define('_DB_ID', 'root');
+define('_DB_PW', 'e27676271');
+define('_DB_NAME', 'tficspika');
 
 $topadmin_array = ['godarasi2767', 'godtest', 'ModAnderson'];
 $admin_array = ['arasi2767', 'testarasi'];
--- heroku-cli-v6.15.31-958455a-linux-x64/bin/heroku.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const {run} = require('@cli-engine/engine')
-const config = {
-  channel: 'stable',
-  version: '6.15.31-958455a'
-}
-run(process.argv, config)
